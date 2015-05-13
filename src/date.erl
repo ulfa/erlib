d39fc456e263d32d074dc89d954c4ffcb4c03f60
Ulf Angermann <ua@innoq.com>
2015-05-13 06:14:49 +0200

    format date

--- src/date.erl
+++ src/date.erl
@@ -19,10 +19,10 @@ get_date_seconds(Date_time) ->
     calendar:datetime_to_gregorian_seconds(Date_time).
 
 get_start_datetime() ->
-	get_formated_date({erlang:date(), {00,00,00}}).
+	get_formated_date(space, {erlang:date(), {00,00,00}}).
 
 get_end_datetime() ->
-	get_formated_date({erlang:date(), {23,59,59}}).
+	get_formated_date(space, {erlang:date(), {23,59,59}}).
 
 is_date_in_range(Akt_date, Date) ->	
 	calendar:datetime_to_gregorian_seconds(Akt_date) - calendar:datetime_to_gregorian_seconds(Date) < 0.
@@ -49,7 +49,6 @@ create_seconds_from_string(Date_time) ->
 	[Y,M,D] = string:tokens(Date, "-"),
 	DT = {{list_to_integer(Y), list_to_integer(M) ,list_to_integer(D)}, {list_to_integer(HH),list_to_integer(MM),list_to_integer(SS)}},
 	get_date_seconds(DT).
-
 
 create_simple_date_from_string([]) ->
 	erlang:date();	
@@ -95,20 +94,29 @@ complete_date(Date) ->
 construct_date({Y, M, D}) ->
 	lists:concat([Y ,"-" ,M ,"-", D]).
 	
-get_formated_date(Date) ->
+get_formated_date(space, Date) ->
 	{{Year,Month,Day},{Hour,Min,Seconds}} = Date,
 	Args = [Year, Month, Day, Hour, Min, Seconds],
 	A = io_lib:format("~B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B", Args),
+	lists:flatten(A);
+
+get_formated_date(minus, Date) ->
+	get_formated_date("~B-~2.10.0B-~2.10.0B_~2.10.0B:~2.10.0B:~2.10.0B", Date);
+
+get_formated_date(Format, Date) ->
+	{{Year,Month,Day},{Hour,Min,Seconds}} = Date,
+	Args = [Year, Month, Day, Hour, Min, Seconds],
+	A = io_lib:format(Format, Args),
 	lists:flatten(A).
 
 get_formated_date_for_now(0) ->
 	get_formated_date_for_now(now());
 
 get_formated_date_for_now(Now) ->
-	get_formated_date(calendar:now_to_local_time(Now)).
+	get_formated_date(space, calendar:now_to_local_time(Now)).
 
 get_formated_date_for_seconds(Seconds) ->
-	get_formated_date(calendar:gregorian_seconds_to_datetime(Seconds)).
+	get_formated_date(minus, calendar:gregorian_seconds_to_datetime(Seconds)).
 
 get_first_day({Y, M, _D}) ->
 	date_lib:create_date_string({{Y, M, 1}, {0,0,0}}).
@@ -133,7 +141,7 @@ seconds_to_date(Seconds) when is_list(Seconds) ->
 	date:get_formated_date(calendar:gregorian_seconds_to_datetime(erlang:list_to_integer(Seconds)));
 
 seconds_to_date(Seconds) when is_integer(Seconds) ->
-	date:get_formated_date(calendar:gregorian_seconds_to_datetime(Seconds)).
+	get_formated_date(space, calendar:gregorian_seconds_to_datetime(Seconds)).
 
 format_uptime(UpTime) ->
 	{D, {H, M, S}} = calendar:seconds_to_daystime(UpTime div 1000),
@@ -182,7 +190,7 @@ create_date_string_test() ->
 create_date_german_string_test() ->
 		?assertEqual("20.10.2012", create_date_german_string({{2012,10,20}, {0,0,0}})).
 get_formated_date_test() ->
-	?assertEqual("2013-05-23 11:39:02", get_formated_date({{2013,5,23},{11,39,2}})).
+	?assertEqual("2013-05-23 11:39:02", get_formated_date(space, {{2013,5,23},{11,39,2}})).
 
 get_formated_date_for_now_test() ->
 	?assertEqual("2013-05-23 11:44:05", get_formated_date_for_now({1369,302245,468365})).
@@ -190,8 +198,7 @@ get_formated_date_for_now_test() ->
 seconds_to_date_test() ->
 	?assertEqual("2015-05-07 06:40:36", seconds_to_date(63598200036)).	
 
-get_formated_date_for_seconds_test() ->
-get_formated_date_for_seconds(63598200036).
-	%%?assertEqual("2015-05-07 06:40:36", get_formated_date_for_seconds(63598200036)).
+get_formated_date_for_seconds_test() ->	
+	?assertEqual("2015-05-07_06:40:36", get_formated_date_for_seconds(63598200036)).
 
 -endif.
